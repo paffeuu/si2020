25f839d4afe23807f080bcbe3d3570a3cf65fc53
paffeuu <paffciek@gmail.com>
2020-04-14 17:04:11 +0200

    unfinished but working evaluation algorithm (square of twos and thress)

--- 3_GAME/test/si/lista3/engine/Connect4EngineTest.java
+++ 3_GAME/test/si/lista3/engine/Connect4EngineTest.java
@@ -241,6 +241,41 @@ public class Connect4EngineTest {
 
     }
 
+    @Test
+    public void evaluateStageForPlayerHorizontal() {
+        engine = new Connect4Engine();
+        engine.nextMove(0,1);
+        engine.nextMove(0,2);
+        engine.nextMove(1,1);
+        engine.nextMove(1,2);
+        engine.nextMove(3,1);
+        engine.nextMove(3,2);
+        engine.nextMove(4,1);
+        engine.nextMove(2,2);
+        engine.printStage();
+        assertEquals(4, engine.evaluateStageForPlayer(1));
+        assertEquals(17, engine.evaluateStageForPlayer(2));
+    }
+
+
+    @Test
+    public void evaluateStageForPlayerMixed() {
+        engine = new Connect4Engine();
+        engine.nextMove(4,1);
+        engine.nextMove(3,2);
+        engine.nextMove(5,1);
+        engine.nextMove(4,2);
+        engine.nextMove(5,1);
+        engine.nextMove(2,2);
+        engine.nextMove(6,1);
+        engine.nextMove(0,2);
+        engine.nextMove(6,1);
+        engine.nextMove(0,2);
+        engine.printStage();
+        assertEquals(12, engine.evaluateStageForPlayer(1));
+        assertEquals(17, engine.evaluateStageForPlayer(2));
+    }
+
 
 
 }
